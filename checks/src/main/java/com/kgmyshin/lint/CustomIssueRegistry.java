8a333165a42ed54ac80c383058914b522781def5
kgmyshin <kgmyshin82@gmail.com>
2018-04-26 18:32:04 +0900

    use CURRENT_API

--- checks/src/main/java/com/kgmyshin/lint/CustomIssueRegistry.java
+++ checks/src/main/java/com/kgmyshin/lint/CustomIssueRegistry.java
@@ -10,6 +10,12 @@ import java.util.List;
  * The list of issues that will be checked when running <code>lint</code>.
  */
 public class CustomIssueRegistry extends IssueRegistry {
+
+    @Override
+    public int getApi() {
+        return com.android.tools.lint.detector.api.ApiKt.CURRENT_API;
+    }
+
     @Override
     public List<Issue> getIssues() {
         return Collections.singletonList(NotHandledDisposableDetector.ISSUE);
